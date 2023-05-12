107878abd33108964dfde0ef7ef966c0602b098a
Quan Nguyen <119392873+QuanNguyen-1st@users.noreply.github.com>
2023-05-12 15:12:13 +0700

    + fix guard behaviour

--- Assets/_Scripts/Grid Environment/Enemies/GuardBahaviour.cs
+++ Assets/_Scripts/Grid Environment/Enemies/GuardBahaviour.cs
@@ -12,6 +12,7 @@ public class GuardBahaviour : MonoBehaviour
     private Vector3 _originalPos;
     [SerializeField] private float _maxDisToOriginalPos;
     [SerializeField] private LayerMask _playerLayerMask;
+    private Transform _target;
     
     // Start is called before the first frame update
     void Start()
@@ -22,24 +23,31 @@ public class GuardBahaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        HandleEnemyBehaviour(DetectPlayer()?.gameObject.transform);
+        DetectPlayer();
+        Behaviour();
     }
-    private Collider2D DetectPlayer() {
-        return Physics2D.OverlapCircle(transform.position, _detectRange, _playerLayerMask);
+    private void DetectPlayer() {
+        Collider2D playerCol = Physics2D.OverlapCircle(transform.position, _detectRange, _playerLayerMask);
+        if (playerCol) {
+            _target = playerCol.gameObject.transform;
+        } else {
+            _target = null;
+        }
     }
     private void OnDrawGizmos() {
         Gizmos.DrawWireSphere(transform.position, _detectRange);
     }
-    private void HandleEnemyBehaviour(Transform target) {
-        if (Vector2.Distance(transform.position, _originalPos) > _maxDisToOriginalPos) {
-            transform.position = Vector2.MoveTowards(transform.position, _originalPos, _speed*Time.deltaTime);
-        }
-        else {
-            if (target != null) {
-                if (Vector2.Distance(transform.position, target.position) > _attackRange) {
-                    transform.position = Vector2.MoveTowards(transform.position, target.position, _speed*Time.deltaTime);
-                }
+    private void Behaviour() {
+        if (_target != null) {
+            if (Vector2.Distance(transform.position, _target.position) <= _maxDisToOriginalPos && Vector2.Distance(transform.position, _target.position) > _attackRange) {
+                transform.position = Vector2.MoveTowards(transform.position, _target.position, _speed*Time.deltaTime);
+                
             }
+            else if (Vector2.Distance(transform.position, _target.position) > _maxDisToOriginalPos) {
+                transform.position = Vector2.MoveTowards(transform.position, _originalPos, _speed*Time.deltaTime);
+            }
+        } else {
+            transform.position = Vector2.MoveTowards(transform.position, _originalPos, _speed*Time.deltaTime);
         }
     }
 }
